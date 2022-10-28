1d03eb2c14acf23e5b70f66c5f26b95cacf28b31
Clarkden <63682937+Clarkden@users.noreply.github.com>
2022-10-28 11:18:05 -0700

    Lazy Loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,9 +48,11 @@ const Home = (props: productProps) => {
         <div className="w-1/3 h-full hidden md:block"></div>
         <div className="md:w-1/3 h-full md:border-x-2 md:border-white/50 flex flex-col justify-center items-center">
           <div className="bg-[#e8eddf] w-4/5 p-3">
-            <img src="/mad_vibes_logo.png"></img>
+            <img loading="lazy" src="/mad_vibes_logo.png"></img>
             <a href="/collections">
-              <h3 className="text-center uppercase border-2 border-black mt-5 rounded-md bg-black text-white hover:scale-[101%] transition">Shop mad vibes</h3>
+              <h3 className="text-center uppercase border-2 border-black mt-5 rounded-md bg-black text-white hover:scale-[101%] transition">
+                Shop mad vibes
+              </h3>
             </a>
           </div>
         </div>
@@ -58,6 +60,7 @@ const Home = (props: productProps) => {
       </section>
       <Parallax speed={100} className="z-50 absolute">
         <img
+          loading="lazy"
           src="flying-bats.png"
           className="w-auto h-[40vh] md:h-[60vh] top-0 left-20"
         />
@@ -70,6 +73,7 @@ const Home = (props: productProps) => {
           {/* <div className="w-full md:w-1/2 flex flex-row flex-wrap mt-8 md:mt-0"> */}
           <div className="w-full md:w-1/4 p-4">
             <img
+              loading="lazy"
               src="/FullSizeRender.JPG"
               className="rounded-sm shadow-md shadow-orange-400 border-2 border-orange-400 saturate-150"
             ></img>
@@ -91,6 +95,7 @@ const Home = (props: productProps) => {
               Shop Collections
             </button>
             <img
+              loading="lazy"
               src="pumpkin1.png"
               className="absolute w-14 h-14  left-6 -bottom-3 md:w-20 md:h-20 md:bottom-4 md:left-20 z-30"
             />
@@ -103,6 +108,7 @@ const Home = (props: productProps) => {
           </div>
           <div className="w-full md:w-1/4 p-4">
             <img
+              loading="lazy"
               src="/IMG_4599.JPG"
               className="rounded-sm shadow-md shadow-orange-400 border-2 border-orange-400 saturate-150"
             ></img>
@@ -110,36 +116,63 @@ const Home = (props: productProps) => {
         </section>
         <div className="border-2 border-neutral-800 w-11/12 mx-auto rounded-lg"></div>
         <section className="h-fit w-full md:h-[80vh] md:w-[95%] mx-auto py-20 md:px-5 flex flex-col md:flex-row gap-8">
-          <div onClick={() => router.push('/collections/beige-on-white-puff-vibe-tee')} className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer">
+          <div
+            onClick={() =>
+              router.push("/collections/beige-on-white-puff-vibe-tee")
+            }
+            className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer"
+          >
             <img
+              loading="lazy"
               src="IMG_8029.jpg"
               className="absolute top-[-9999px] bottom-[-9999px] left-[-9999px] right-[-9999px] m-auto min-h-full min-w-full rounded-sm"
             ></img>
           </div>
-          <div onClick={() => router.push('/collections/mad-vibes-trucker-hat-camo')} className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer">
+          <div
+            onClick={() =>
+              router.push("/collections/mad-vibes-trucker-hat-camo")
+            }
+            className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer"
+          >
             <img
+              loading="lazy"
               src="IMG_1000.png"
               className="absolute top-[-9999px] bottom-[-9999px] left-[-9999px] right-[-9999px] m-auto min-h-full min-w-full rounded-sm"
             ></img>
           </div>
-          <div onClick={() => router.push('/collections/blackout-puff-vibe-tee')} className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer">
+          <div
+            onClick={() => router.push("/collections/blackout-puff-vibe-tee")}
+            className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer"
+          >
             <img
+              loading="lazy"
               src="IMG_8088.jpg"
               className="absolute top-[-9999px] bottom-[-9999px] left-[-9999px] right-[-9999px] m-auto min-h-full min-w-full rounded-sm"
             ></img>
           </div>
-          <div onClick={() => router.push('/collections/blackout-puff-vibe-tee')} cursor-pointer className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer">
+          <div
+            onClick={() => router.push("/collections/blackout-puff-vibe-tee")}
+            cursor-pointer
+            className="w-10/12 mx-auto h-[50vh] md:h-full md:w-1/4 overflow-hidden relative shadow-white shadow-md border-2 border-white hover:shadow-lg hover:shadow-white transition cursor-pointer"
+          >
             <img
+              loading="lazy"
               src="IMG_7920.jpg"
               className="absolute top-[-9999px] bottom-[-9999px] left-[-9999px] right-[-9999px] m-auto min-h-full min-w-full rounded-sm"
             ></img>
           </div>
-          
         </section>
         <div className="border-2 border-neutral-800 w-11/12 mx-auto rounded-lg"></div>
         <section className="h-fit md:h-[15vh] my-20 mb-24 flex flex-col items-center justify-center w-fit mx-auto">
-          <h1 className="text-xl md:text-3xl text-white">Shop our latest drops now</h1>
-          <button className="bg-rose-500 p-2 md:p-3 px-4 rounded-lg mt-3 text-white w-full text-base md:text-lg hover:scale-105 ease-in-out transition" onClick={() => router.push('/collections')}>Shop Collections</button>
+          <h1 className="text-xl md:text-3xl text-white">
+            Shop our latest drops now
+          </h1>
+          <button
+            className="bg-rose-500 p-2 md:p-3 px-4 rounded-lg mt-3 text-white w-full text-base md:text-lg hover:scale-105 ease-in-out transition"
+            onClick={() => router.push("/collections")}
+          >
+            Shop Collections
+          </button>
         </section>
       </main>
     </>
